2ffbe566d6513eea8131daab03e71e73e39aea66
Kim Jin-yeong <jinzero29@gmail.com>
2022-05-05 03:47:30 +0900

    Add license, readme

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ ax1 = fig.add_subplot(3, 1, 1)
 ax2 = fig.add_subplot(3, 1, 3)
 
 line1, = ax1.plot(x, Sinfunc(x, 1, 1), linestyle="--", label="Traveling Wave_1")
-line2, = ax1.plot(x, Sinfunc(x, -1, 2), linestyle="--", label="Traveling Wave_2")
+line2,ds = ax1.plot(x, Sinfunc(x, -1, 2), linestyle="--", label="Traveling Wave_2")
 line3, = ax1.plot(x, Sinfunc(x, 1, 1) + Sinfunc(x, -1, 2), label="Standing Wave")
 line4, = ax2.plot(x, Sinfunc(x, 1, 1) + Sinfunc(x, -1, 2), label="Standing Wave")
 
