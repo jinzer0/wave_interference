dd6d63aa6b174f08b830d190cf4006f9edd9faf7
Kim Jin-yeong <jinzero29@gmail.com>
2022-06-30 14:47:02 +0900

    some revise

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ ax1 = fig.add_subplot(3, 1, 1)
 ax2 = fig.add_subplot(3, 1, 3)
 
 line1, = ax1.plot(x, Sinfunc(x, 1, 1), linestyle="--", label="Traveling Wave_1")
-line2,ds = ax1.plot(x, Sinfunc(x, -1, 2), linestyle="--", label="Traveling Wave_2")
+line2, = ax1.plot(x, Sinfunc(x, -1, 2), linestyle="--", label="Traveling Wave_2")
 line3, = ax1.plot(x, Sinfunc(x, 1, 1) + Sinfunc(x, -1, 2), label="Standing Wave")
 line4, = ax2.plot(x, Sinfunc(x, 1, 1) + Sinfunc(x, -1, 2), label="Standing Wave")
 
@@ -25,12 +25,12 @@ ax1.set_ylim(3, -3)
 ax2.set_ylim(3, -3)
 plt.ion()
 
-for a in np.arange(1, 50, 0.1):
+for a in np.arange(40, 50, 0.1):
     # plt.axhline(y=0, color="black")
     line1.set_ydata(Sinfunc(x, a, 1))
     line2.set_ydata(Sinfunc(x, -a, b))
     line3.set_ydata(Sinfunc(x, a, 1) + Sinfunc(x, -a, b))
-    line4.set_ydata(Sinfunc(x, a, 1) + Sinfunc(x, -a, 1))
+    line4.set_ydata(Sinfunc(x, a, 1) + Sinfunc(x, -a, b))
 
     if b > 1:
         b -= 0.01
